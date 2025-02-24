dc2fc2d72b30ba6cb9af879fad2da3273de91b05
nijamuddinalam23 <nijamuddinalam23@gmail.com>
2025-02-25 01:44:33 +0530

    first project

---
+++ script.js
@@ -0,0 +1,39 @@
+const start = document.getElementById('start');
+const pause = document.getElementById('pause');
+const reset = document.getElementById('reset');
+const timer = document.getElementById('timer');
+const userTime = document.getElementById('time');
+
+let minutes = 0;
+let second = 0;
+let timerId ;
+
+function userInput (){
+  minutes = userTime.value;
+  timer.innerHTML = userTime.value + " " + "min";
+  userTime.value === "0"
+}
+start.addEventListener('click', ()=>{
+
+  timerId = setInterval(() => {
+    if(second === 0){
+      if(minutes !== 0){
+        minutes--;
+        second = 59;
+      }
+    }
+    else{
+      second--;
+    }
+   timer.textContent = `${minutes}:${second>=10 ? second : `0${second}`}`;
+  }, 1000);
+  return timerId;
+})
+pause.addEventListener('click',()=>{
+  clearTimeout(timerId);
+});
+reset.addEventListener('click' , ()=>{
+  minutes =25;
+  second = 0;
+  timer.innerHTML = `${minutes} :${second>=10 ? second : `0${second}`}`
+})
